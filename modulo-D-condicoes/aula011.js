c577d97845d846cc9b9e763ebc401cbfff5acc92
joaomachadobr <214250129+joaomachadobr@users.noreply.github.com>
2025-07-18 22:03:36 +0100

    estudos sobre condicoes

---
+++ modulo-D-condicoes/aula011.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log(`A velocidade do carro e de ${vel}km/h`)
+if (vel > 60) {
+    console.log('Voce foi multado!')
+}
+console.log('Dirija sempre usando o cinto de seguranca!')
+
+
---
+++ modulo-D-condicoes/ex001.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais != 'EUA') {
+    console.log('You are a foreing!')
+} else {
+   console.log('You are a patriot!') 
+}
