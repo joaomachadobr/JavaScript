a2223a85bf160240c7e1cb2daf2917554c88416d
joaomachadobr <214250129+joaomachadobr@users.noreply.github.com>
2025-07-22 18:25:30 +0100

    repeticoes

---
+++ modulo-E-repeticao/ex01.js
@@ -0,0 +1,15 @@
+var c = 1
+
+//TESTE NO COMECO
+/*
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+// TESTE NO FINAL
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 8);
